b52b07aac92f80297974f475ea931df442c76efe
puripuri2100 <puripuri2100@gmail.com>
2020-03-15 16:27:10 +0900

    fix

---
+++ src/func.rs
@@ -0,0 +1,60 @@
+use numext_fixed_uint::U2048;
+
+//nPr
+pub fn permutation(n64: u64, r: u64) -> U2048 {
+  let n: U2048 = n64.into();
+  let mut m: U2048 = U2048::from(1 as u64);
+  for i_64 in 0..r {
+    let i: U2048 = i_64.into();
+    let l: U2048 = m * (n.clone() - i);
+    m = l
+  }
+  m
+}
+
+// nCr
+// 4C3 = 4*3*2
+pub fn combin(n64: u64, r: u64) -> U2048 {
+  let n2: U2048 = permutation(n64, r).into();
+  let r2: U2048 = permutation(r, r).into();
+  let a = n2 / r2;
+  println!("combin {}C{} -> {}", n64, r, a.clone());
+  a
+}
+
+//n!
+pub fn factorial(n: u64) -> U2048 {
+  let mut m: U2048 = U2048::from(1 as u64);
+  for i_64 in 1..(n + 1) {
+    let i: U2048 = i_64.into();
+    m = m * i;
+    //println!("m : {}", m)
+  }
+  println!("factorial : {}! -> {}", n, m);
+  m
+}
+
+pub fn number_of_combinations(all: u64, group: u64) -> U2048 {
+  // (400C50 * 350C50 * 300C50 * 250C50 * 200C50 * 150C8 * 100C50) / 50!
+  let member = all / group;
+  let mut n: U2048 = U2048::from(1 as u64);
+  for i in 0..group {
+    let nokori = all - member * i;
+    n = n * combin(nokori, member);
+    //println!("n : {}", n);
+  }
+  println!("main : {}", n);
+  let l = factorial(group);
+  n / l
+}
+
+#[test]
+
+fn check() {
+  assert_eq!(combin(4, 2), U2048::from(6 as u64));
+  assert_eq!(combin(6, 2), U2048::from(15 as u64));
+  assert_eq!(combin(9, 3), U2048::from(280 as u64));
+
+  assert_eq!(factorial(5), U2048::from(120 as u64));
+  assert_eq!(factorial(6), U2048::from(720 as u64));
+}
